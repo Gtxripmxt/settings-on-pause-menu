d6bd7f47d4540e4145423b0843fcc524016e8c5a
Gtxripmxt <gtxripmxt@gmail.com>
2025-05-13 19:10:55 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,31 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PauseLayer.hpp>
-#include <Geode/ui/GeodeUI.hpp>
-#include <Geode/utils/cocos.hpp>
 #include <Geode/binding/OptionsLayer.hpp>
 
 using namespace geode::prelude;
 
-class $modify(PauseWithImageButton, PauseLayer) {
+class $modify(MyPauseLayer, PauseLayer) {
 public:
-    void customSetup() override;
+    void MyPauseLayer() override;
     void onSettingsButton(cocos2d::CCObject*);
 };
 
 void PauseWithImageButton::customSetup() {
     PauseLayer::customSetup();
 
-    auto sprite = CCSprite::createWithSpriteFrameName("GJ_optionsBtn_001.png");
-
     auto button = CCMenuItemSpriteExtra::create(
-        sprite,
-        sprite,
-        this,
-        menu_selector(PauseWithImageButton::onSettingsButton)
+			CCSprite::createWithSpriteFrameName("GJ_infoBtn_001.png"),
+			this,
+			menu_selector(MyPauseLayer::onInfoButton)
     );
+	auto menu = this->getChildByID("center-button-menu");
+	button->setID("settings-button"_spr);
+	menu->addChild(button);
 
-    auto winSize = cocos2d::CCDirector::sharedDirector()->getWinSize();
-    button->setPosition({ winSize.width - 40.f, winSize.height / 2 });
-
-    auto menu = cocos2d::CCMenu::create();
-    menu->addChild(button);
-    menu->setPosition({ 0, 0 });
-
-    this->addChild(menu);
+	menu->updateLayout();
 };
-
-void PauseWithImageButton::onSettingsButton(cocos2d::CCObject*) {
-           auto options = OptionsLayer::create();
+void MyPauseLayer::onSettingsButton(cocos2d::CCObject*) {
+    auto options = OptionsLayer::create();
     if (!options) return;
     auto scene = cocos2d::CCDirector::sharedDirector()->getRunningScene();
     if (!scene) return;
