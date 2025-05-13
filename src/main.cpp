0f5112deed23163ea6e9bbdc5df0c335ca80fbb6
Gtxripmxt <gtxripmxt@gmail.com>
2025-05-13 19:27:03 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,30 +6,27 @@ using namespace geode::prelude;
 
 class $modify(MyPauseLayer, PauseLayer) {
 public:
-    void MyPauseLayer() override;
-    void onSettingsButton(cocos2d::CCObject*);
-};
+    void customSetup() {
+        PauseLayer::customSetup();
 
-void MyPauseLayer::customSetup() {
-    PauseLayer::customSetup();
-
-    auto button = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_infoBtn_001.png"),
-			this,
-			menu_selector(MyPauseLayer::onSettingsButton)
-    );
+        auto button = CCMenuItemSpriteExtra::create(
+	    CCSprite::createWithSpriteFrameName("GJ_infoBtn_001.png"),
+	    this,
+	    menu_selector(MyPauseLayer::onSettingsButton)
+        );
 	auto menu = this->getChildByID("center-button-menu");
 	button->setID("settings-button"_spr);
 	menu->addChild(button);
 
 	menu->updateLayout();
+    }
+    void MyPauseLayer::onSettingsButton(cocos2d::CCObject*) {
+        auto options = OptionsLayer::create();
+        if (!options) return;
+        auto scene = cocos2d::CCDirector::sharedDirector()->getRunningScene();
+        if (!scene) return;
+        int zOrder = 1000;
+        scene->addChild(options, zOrder);
+        options->showLayer(false);	
+    }
 };
-void MyPauseLayer::onSettingsButton(cocos2d::CCObject*) {
-    auto options = OptionsLayer::create();
-    if (!options) return;
-    auto scene = cocos2d::CCDirector::sharedDirector()->getRunningScene();
-    if (!scene) return;
-    int zOrder = 1000;
-    scene->addChild(options, zOrder);
-    options->showLayer(false);	
-}
